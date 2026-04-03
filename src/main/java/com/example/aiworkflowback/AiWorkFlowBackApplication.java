f4497a979e8112683dafad096dfda0ea786b549d
BestSmile <12709213+lubensheng@user.noreply.gitee.com>
2026-04-03 12:17:19 +0800

    feature: 初始化项目

---
+++ src/main/java/com/example/aiworkflowback/AiWorkFlowBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.aiworkflowback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiWorkFlowBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiWorkFlowBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aiworkflowback/AiWorkFlowBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aiworkflowback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiWorkFlowBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
